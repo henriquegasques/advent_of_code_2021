9a34f06071858a3bdbbd6cdb4ca39635f3cccf64
Henrique Gasques <h.gasques@paywith.com>
2021-12-03 00:50:15 -0300

    Day One Part Two solution in Elixir

--- day_2/solution.exs
+++ day_2/solution.exs
@@ -1,27 +1,54 @@
 defmodule Solution do
   def part_one do
-    positions = %{ horizontal: 0, depth: 0 }
+    position = %{ horizontal: 0, depth: 0 }
 
-    File.read!("input.txt")
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.split/1)
-    |> Enum.reduce(positions, &update_position/2)
+    instructions()
+    |> Enum.reduce(position, &calculate_position/2)
     |> Map.values
     |> Enum.reduce(&(&1 * &2))
     |> IO.puts
   end
 
-  defp update_position(["forward", amount], position) do
+  def part_two do
+    position = %{ aim: 0, horizontal: 0, depth: 0 }
+
+    instructions()
+    |> Enum.reduce(position, &calculate_position_with_aim/2)
+    |> fn %{ horizontal: horizontal, depth: depth, aim: _aim } -> horizontal * depth end.()
+    |> IO.puts
+  end
+
+  defp calculate_position(["forward", amount], position) do
     %{ position | horizontal: position[:horizontal] + String.to_integer(amount) }
   end
 
-  defp update_position(["down", amount], position) do
+  defp calculate_position(["down", amount], position) do
     %{ position | depth: position[:depth] + String.to_integer(amount) }
   end
 
-  defp update_position(["up", amount], position) do
+  defp calculate_position(["up", amount], position) do
     %{ position | depth: position[:depth] - String.to_integer(amount) }
+  end
+
+  defp calculate_position_with_aim(["forward", amount_string], position) do
+    amount = String.to_integer(amount_string)
+    %{ position | horizontal: position[:horizontal] + amount, depth: position[:depth] + amount * position[:aim] }
+  end
+
+  defp calculate_position_with_aim(["down", amount], position) do
+    %{ position | aim: position[:aim] + String.to_integer(amount) }
+  end
+
+  defp calculate_position_with_aim(["up", amount], position) do
+    %{ position | aim: position[:aim] - String.to_integer(amount) }
+  end
+
+  defp instructions do
+    File.read!("input.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split/1)
   end
 end
 
 Solution.part_one()
+Solution.part_two()
